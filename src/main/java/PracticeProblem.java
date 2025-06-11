ee7c906bc5c0cedbd4ee48f4f5f5606e0e9fac6a
Mayurun-Ravindramoorthi-ics4u1-03-25 <mayurun.ravindramoorthi@student.tdsb.on.ca>
2025-06-11 13:25:13 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,23 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static int towerOfHanoi(int num) {
+        if (num < 3) {
+            return -1; // Invalid case
+        }
+        return hanoiRecursive(num);
+    }
 
-	}
+    private static int hanoiRecursive(int n) {
+        if (n == 1) {
+            return 1; // Base case: 1 disk requires 1 move
+        }
+        return 2 * hanoiRecursive(n - 1) + 1;
+    }
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+    // Optional: quick test
+    public static void main(String[] args) {
+        System.out.println(towerOfHanoi(3)); // Should print 7
+        System.out.println(towerOfHanoi(4)); // Should print 15
+        System.out.println(towerOfHanoi(2)); // Should print -1
+    }
 }
